1ebbf92966272cef9a2231df1f4b6bd57732074f
Krish Patel <kr983475@dal.ca>
2025-01-18 17:15:06 -0400

    springboot setup

---
+++ mindease/src/main/java/com/example/mindease/MindeaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mindease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindeaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindeaseBackendApplication.class, args);
+	}
+
+}
---
+++ mindease/src/test/java/com/example/mindease/MindeaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindeaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
